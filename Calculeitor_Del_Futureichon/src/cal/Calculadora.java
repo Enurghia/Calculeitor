671dfc1f5e4a32d0cce9019875c627567c3660a5
Enurghia <44556187+Enurghia@users.noreply.github.com>
2018-11-26 17:02:57 -0300

    Update grande pero no tanto
    (Por revisar)
    Añadidas varias funciones para cambiar bases de números a la librería BIN.
    - Suma de BINARIOS (int)(String).
    - Suma de HEXADECIMALES(int)(String).
    - BINARIO a DECIMAL.
    - HEXADECIMAL a DECIMAL.
    - N_BASE a DECIMAL.
    Corregido un bug que provocaba que al elevar un N número a 1 el resultado fuera 0.

---
+++ Calculeitor_Del_Futureichon/src/cal/Calculadora.java
@@ -0,0 +1,27 @@
+package cal;
+import java.util.*;
+public class Calculadora {
+	private static Scanner sc;
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		String option = new String();
+		boolean Aux = true;
+		System.out.print("Bienvenido a la Calculeitor del futureichon!"+"\n");
+		while(Aux == true){
+			System.out.print("Ingrese la función que desea."+"\n"+
+			"	1) Calculadora matemática."+"\n"+
+			"	2) Calculadora de programador."+"\n"+"	3) Promedios."+"\n"+
+			"	4) Ordenamiento de datos."+"\n"+"Ingrese [0] y pulse [ENTER] para"+
+			" salir del programa."+"\n");
+			option = sc.nextLine();
+			switch(option){
+			case "1": Cal_Class.cal_Aritm();break;
+			case "2": Cal_Class.cal_Bin();break;
+			case "3": Cal_Class.cal_Prom();break;
+			case "4": Cal_Class.cal_Dat();break;
+			case "0": System.out.println(". . . CERRANDO . . ."); System.exit(0);break;
+			default: System.out.println("Dato inválido, inténtelo otra vez.");break;
+			}
+		}
+	}
+}
