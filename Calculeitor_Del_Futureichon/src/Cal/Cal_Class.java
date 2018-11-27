f882b6fa7a3c96311afb84bb73593e54c5aef9ee
Enurghia <44556187+Enurghia@users.noreply.github.com>
2018-11-26 21:01:14 -0300

    Big Update 02
    (Por revisar)
    Wena cabros:
    Clase Opera (Para operaciones matemáticas) ya debería ser totalmente funcional.
    Clase Bin (Para operaciones de nBase) ya debería ser totalmente funcional.
    Clase Prom (Para operaciones de promedios) aún no se inicia su elaboración.
    Clase Dat (Para operaciones de ordenamientos de datos) aún no se inicia su elaboración.
    Clase Cal_Class (Para la interfaz de la calculadora) se encuentra a la mitad de su desarrollo.
    Calculadora (main) se encuentra a la mitad de su desarrollo.

---
+++ Calculeitor_Del_Futureichon/src/Cal/Cal_Class.java
@@ -0,0 +1,190 @@
+package Cal;
+import java.util.Scanner;
+
+public class Cal_Class {
+	public static void cal_Aritm (){
+		Scanner sc = new Scanner(System.in);
+		double A = 0;
+		double B = 0;
+		String option = new String ();
+		boolean Aux = true;
+		System.out.print("Calculadora aritmética. . . "+"\n");
+		while (Aux == true) {
+			System.out.print("Ingrese la operación."+"\n"+"	+) Adición."+"\n"
+					+ "	-) Sustracción."+"\n"+	"	*) Multiplicación."+"\n"
+					+"	/) División."+"\n"+ "	%) Resto de división."+"\n"
+					+"	^) Potencias."+"\n"
+					+"Ingrese [0] y pulse [ENTER] para"
+					+ " retroceder."+"\n");
+			option = sc.next();
+			switch(option){
+				case "+":
+					System.out.print("Ingrese un valor para someterlo a operación."+"\n");
+					A = sc.nextInt();
+					System.out.println("Ingrese otro valor para continuar la operación.");
+					B = sc.nextInt();
+					System.out.println(A+" + "+B+" = "+(A+B)+"\n");
+					break;
+				case "-":
+					System.out.print("Ingrese un valor para someterlo a operación."+"\n");
+					A = sc.nextDouble();
+					System.out.println("Ingrese otro valor para continuar la operación.");
+					B = sc.nextDouble();
+					System.out.println(A+" - "+B+" = "+(A-B)+"\n");
+					break;
+				case "*":
+					System.out.print("Ingrese un valor para someterlo a operación."+"\n");
+					A = sc.nextDouble();
+					System.out.println("Ingrese otro valor para continuar la operación.");
+					B = sc.nextDouble();
+					System.out.println(A+" * "+B+" = "+(Opera.multiply(A, B))+"\n");
+					break;
+				case "/":
+					System.out.print("Ingrese un valor para someterlo a operación."+"\n");
+					A = sc.nextDouble();
+					System.out.println("Ingrese otro valor para continuar la operación.");
+					B = sc.nextDouble();
+					System.out.println(A+" / "+B+" = "+(Opera.division(A, B))+"\n");
+					break;
+				case "%":
+					System.out.print("Ingrese un valor para someterlo a operación."+"\n");
+					A = sc.nextDouble();
+					System.out.println("Ingrese otro valor para continuar la operación.");
+					B = sc.nextDouble();
+					System.out.println(A+" % "+B+" = "+(Opera.divMod(A, B))+"\n");
+					break;
+				case "^":
+					System.out.print("Ingrese un valor para someterlo a operación."+"\n");
+					A = sc.nextDouble();
+					System.out.println("Ingrese otro valor para continuar la operación.");
+					B = sc.nextDouble();
+					System.out.println(A+" ^ "+B+" = "+(Opera.power(A, B))+"\n");
+					break;
+				case "0": System.out.println(". . . Atras . . ."+"\n"); Aux = false;break;
+				default: System.out.println("Dato inválido, inténtelo otra vez."+"\n");break;
+			}
+		}
+	}
+	public static void cal_Bin () {
+		Scanner sc = new Scanner(System.in);
+		int A1 = 0;
+		int B1 = 0;
+		String A2 = new String ("");
+		String B2 = new String ("");
+		String option = new String ();
+		boolean Aux = true;
+		System.out.print("Calculadora de programador. . . "+"\n");
+		while (Aux == true) {
+			System.out.print("Ingrese la operación."+"\n"+"	BIN) Suma de binarios."+"\n"
+					+ "	HEX) Suma de hexadecimales."+"\n"+	"	CBS) Cambiar de base."+"\n"+
+					"	DAB) Decimal a otras bases."+"\n"
+					+"Ingrese [0] y pulse [ENTER] para"
+					+ " retroceder."+"\n");
+			option = sc.next();
+			switch(option.toUpperCase()){
+				case "BIN": System.out.print("¿Desea sumar números decimales o binarios?"
+											+"\n"+"	DEC) Decimales."
+											+"\n"+"	BIN) Binarios."
+											+"\n");
+					option = sc.next();
+					switch(option.toUpperCase()) {
+						case "DEC":
+							System.out.print("Ingrese un valor para someterlo a operación."+"\n");
+							A1 = sc.nextInt();
+							System.out.println("Ingrese otro valor para continuar la operación.");
+							B1 = sc.nextInt();
+							System.out.print("En Binario es: "+Bin.CambioBase(A1, 2)+" + "
+							+Bin.CambioBase(B1, 2)+" = "+Bin.Suma_BinDec(A1, B1)
+							+"\n"+"En Decimal es: "+A1+" + "+B1+" = "
+							+(A1+B1)+"\n\n");
+							break;
+						case "BIN":
+							System.out.print("Ingrese un valor para someterlo a operación."+"\n");
+							A2 = sc.next();
+							System.out.println("Ingrese otro valor para continuar la operación.");
+							B2 = sc.next();
+							System.out.print("En Binario es: "+A2+" + "+B2+" = "
+							+Bin.CambioBase((Bin.BinADecimal(A2)+Bin.BinADecimal(B2)),2)
+							+"\n"+"En Decimal es: "+Bin.BinADecimal(A2)+" + "+Bin.BinADecimal(B2)
+							+" = "+((Bin.BinADecimal(A2)+Bin.BinADecimal(B2)))
+							+"\n\n");
+							break;
+						default:
+							System.out.println("Dato inválido,intentelo otra vez."+"\n");
+							break;
+					}
+					break;
+				case "HEX":
+					System.out.print("¿Desea sumar números decimales o binarios?"
+							+"\n"+"	DEC) Decimales."
+							+"\n"+"	HEX) Binarios."
+							+"\n");
+					option = sc.next();
+					switch(option.toUpperCase()) {
+					case "DEC":
+						System.out.print("Ingrese un valor para someterlo a operación."+"\n");
+						A1 = sc.nextInt();
+						System.out.println("Ingrese otro valor para continuar la operación.");
+						B1 = sc.nextInt();
+						System.out.print("En Hexadecimal es: "+Bin.CambioBase(A1, 16)+" + "
+						+Bin.CambioBase(B1, 16)+" = "+Bin.Suma_HexDec(A1, B1)
+						+"\n"+"En Decimal es: "+A1+" + "+B1+" = "
+						+(A1+B1)+"\n\n");
+						break;
+					case "HEX":
+						System.out.print("Ingrese un valor para someterlo a operación."+"\n");
+						A2 = sc.next();
+						System.out.println("Ingrese otro valor para continuar la operación.");
+						B2 = sc.next();
+						System.out.print("En Hexadecimal es: "+A2+" + "+B2+" = "
+						+Bin.CambioBase((Bin.HexADecimal(A2)+Bin.HexADecimal(B2)),16)
+						+"\n"+"En Decimal es: "+Bin.HexADecimal(A2)+" + "+Bin.HexADecimal(B2)+" = "
+						+((Bin.HexADecimal(A2)+Bin.HexADecimal(B2)))
+						+"\n\n");
+						break;
+					default:
+						System.out.println("Dato inválido,intentelo otra vez."+"\n");
+						break;
+						}
+					break;
+				case "CBS":
+					System.out.print("Ingrese el valor el cual desea transformar."+"\n");
+					A2 = sc.next();
+					System.out.print("Ingrese la base del valor (el rango permitido es de 2 a 16)."+"\n");
+					A1 = sc.nextInt();
+					System.out.println("Ingrese la base a la cual se quiere cambiar (el rango permitido es de 2 a 16).");
+					B1 = sc.nextInt();
+					System.out.println("("+A2.toUpperCase()	+")"+A1+" a ()"+B1+" es ("+(Bin.CambioBase(Bin.BaseADecimal(A2, A1), B1))+")"+B1+"\n");
+					break;
+				case "DAB":
+					System.out.print("Ingrese la base a la que se desea cambiar."+"\n"
+							+"	BIN) Binario."+"\n"+"	HEX) Hexadecimal."+"\n");
+					A2 = sc.next();
+					switch (A2.toUpperCase()){
+					case "BIN":
+						System.out.print("Ingrese la base del valor a transformar."+"\n");
+						A1 = sc.nextInt();
+						System.out.println("("+A1+")"+"10"+" a "
+						+"()2"+" es ("+Bin.DecimalABin(A1)+")");
+						break;
+					case "HEX":
+						System.out.print("Ingrese la base del valor a transformar."+"\n");
+						A1 = sc.nextInt();
+						System.out.println("("+A1+")"+"10"+" a "
+						+"()16"+" es ("+Bin.DecimalAHex(A1)+")");
+						break;
+					default: System.out.println("Base no válida."+"\n");break;
+					}
+					break;
+				case "0": System.out.println(". . . Atras . . ."+"\n"); Aux = false;break;
+				default: System.out.println("Dato inválido, inténtelo otra vez."+"\n");break;
+			}
+		}
+	}
+	public static void cal_Prom () {
+		Scanner sc = new Scanner(System.in);
+	}
+	public static void cal_Dat () {
+		Scanner sc = new Scanner(System.in);
+	}
+}
---
+++ Calculeitor_Del_Futureichon/src/Cal/Opera.java
@@ -0,0 +1,151 @@
+package Cal;
+public class Opera {
+	public static double multiply (double Factor1, double Factor2){
+		/*Función para discriminar los factores de una multiplicación y para invocar la multiplicación
+		que se deba desarrollar.*/
+		//Factor1 y Factor2 son los factores de la multiplicación.
+		double resultado = 0;
+		String Factor2S = new String ("" + Factor2);
+		/*En caso de ingresar un segundo factor decimal, este se multiplicará por 10
+		y el primer factor se dividirá en 10 tantas veces como decimales tenga el
+		segundo factor.*/ 
+		while((Factor2 - (int)Factor2) != (0)) {
+			Factor2S = String.format("%.7g%n", multiply(Factor2, 10));
+			Factor1 = division(Factor1,10);
+			Factor2 = Double.valueOf(Factor2S.replace(",", "."));
+		}
+		if (Factor2 >= 0){
+			resultado = multiply_complement (Factor1, Factor2);
+			return resultado;
+		}
+		else{
+			Factor2 = -Factor2;
+			if(Factor1 >= 0){
+				resultado = multiply_complement (Factor1, Factor2);
+				return -resultado;
+			}
+			else{
+				Factor1 = -Factor1;
+				resultado = multiply_complement (Factor1, Factor2);
+				return resultado;
+			}
+		}
+	}
+	private static double multiply_complement (double Factor1, double Factor2){
+		double total = 0;
+		for(int i = 0; i < Factor2; i++){
+			total += Factor1;
+		}
+		return total;
+	}
+	public static double division (double div1, double div2){
+		double resultado = 0;
+		if (div2 >= 0){
+			if(div1 < 0){
+				div1 = -div1;
+				resultado = division_complement (div1, div2);
+				return -resultado;
+			}
+			resultado = division_complement (div1, div2);
+			return resultado;
+		}
+		else{
+			div2 = -div2;
+			if(div1 >= 0){
+				resultado = division_complement (div1, div2);
+				return -resultado;
+			}
+			else{
+				div1 = -div1;
+				resultado = division_complement (div1, div2);
+				return resultado;
+			}
+		}
+	}
+	private static double division_complement (double div1, double div2){
+		double total = 0;
+		int aux = 0;
+		String totalS = new String ("");
+		// while(div1 > div2){
+		// >= eso corrijo el error.
+		if(div2 == 0){
+			return 0;
+		}
+		else{
+			while(div1 >= div2){
+				div1 -= div2;
+				aux++;
+			}
+			if(div1 == 0){
+				return aux;
+			}
+			else{
+				totalS += aux + ".";
+				while (div1 != 0){
+					div1 = multiply(div1, 10);
+					aux = 0;
+					while(div1 >= div2){
+						div1 -= div2;
+						aux++;
+					}
+					totalS += aux;
+					if ((totalS.length()) == 15){
+						break;
+					}
+				}
+				total = Double.valueOf(totalS);
+				return total;
+			}
+		}
+	}
+	public static double power (double base, double exponente){
+		double respuesta = 0;
+		double aux = base;
+		if ((int)exponente == 0){
+			return 1;
+		}
+		else{
+			if ((int)exponente > 0){
+				for(int i = 0; i < (int)exponente; i++){
+					if(i == 0) {
+						respuesta = base;
+					}
+					else {
+						base = multiply(base, aux);
+						respuesta = base;
+					}
+				}
+				return respuesta;
+			}
+			else{
+				exponente = -exponente;
+				respuesta = division(1, power(base, (int)exponente));
+				return respuesta;
+			}
+		}
+	}
+	public static double divMod (double div1, double div2){
+		double resultado = divMod_complement(div1, div2);
+		return resultado;
+	}
+	private static double divMod_complement (double div1, double div2){
+		if(div2 == 0){
+			return 0;
+		}
+		else{
+			while(div1 >= div2){
+				div1 -= div2;
+			}
+			return div1;
+		}
+	}
+	public static double round (double valor){
+		if (multiply((valor)-((int)valor),10) >= 5){
+			valor = (int)valor + 1;
+		}
+		else{
+			valor = (int)valor;
+		}
+		return valor;
+	}
+}
