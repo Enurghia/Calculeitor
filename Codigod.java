cef212d14b28613f136e8f937b99c016814220f4
Enurghia <44556187+Enurghia@users.noreply.github.com>
2018-11-30 20:05:39 -0300

    Prueba 02 prom

---
+++ Codigod.java
@@ -0,0 +1,78 @@
+package Cal;
+import java.util.*;
+public class Codigod {
+	private static Scanner sc;
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		double numero = 0;
+		double [][][]Promedios = new double [10][100][25];
+		System.out.print("El límite de cursos es de 10."
+				+"\n"+"El límite de estudiantes es de 100."
+				+"\n"+"El límite de notas es de 25."
+				+"\n");
+		String num = new String ("");
+		int c1 = 0;
+		int c2 = 0;
+		int c3 = 0;
+		int [] aux1 = new int [25];
+		int [] aux2 = new int [100];
+		int [] aux3 = new int [10];
+		for(c1 = 0; c1 < 10; c1++) {
+			for(c2 = 0; c2 < 100; c2++){
+				for(c3 = 0; c3 < 25; c3++){
+					System.out.print("Ingrese la cantidad de notas de los estudiantes."
+					+"\n"+"Una vez acabe de ingresar notas, escriba [END]"
+					+ " y presione [ENTER]"+"\n"+"Lleva "+aux1+" de 25"
+					+ " notas ingresados."+"\n");
+					num = sc.next();
+					num = num.toUpperCase();
+					if(num.equals("END")) {
+						break;
+					}
+					else {
+						numero = Prom.Delta(num);
+						if (numero == 0) {
+							System.out.println("Nota no válida");
+							c3 -= 1;
+						}
+						else {
+							Promedios[c1][c2][c3] = numero;
+							aux1[c3] += 1;
+						}
+					}
+				}
+				System.out.println("Si acabó de ingresar estudiantes, escriba [END]"
+				+ " y presione [ENTER]"+"\n"+"Si no ha acabado, ingrese cualquier"
+				+ " caracter y presione [ENTER]"+"\n"+"Lleva "+aux2+" de 100"
+				+ " estudiantes."+"\n");
+				num = sc.next();
+				num = num.toUpperCase();
+				if(num.equals("END")) {
+					break;
+				}
+				else {
+					aux2[c2] += 1;
+				}
+			}
+			System.out.println("Una vez acabe de ingresar el curso, escriba [END]"
+					+ " y presione [ENTER]"+"\n"+"Si no ha acabado, ingrese cualquier"
+					+ " caracter y presione [ENTER]"+"\n"+"Lleva "+aux3+" de 10"
+					+ " cursos."+"\n");
+			num = sc.next();
+			num = num.toUpperCase();
+			if(num.equals("END")) {
+				break;
+			}
+			else {
+				aux3[c1] += 1;
+			}
+		}
+		for(c1 = 0; c1 < aux3[c1]; c1++) {
+			for(c2 = 0; c2 < aux2[c2]; c2++) {
+				for(c3 = 0; c3 < aux1[c3]; c3++) {
+					System.out.println(Promedios[c1][c2][c3]);
+				}
+			}
+		}
+	}
+}
--- Prom.java
+++ Prom.java
@@ -1,7 +1,5 @@
 package Cal;
-
 import java.util.Scanner;
-
 public class Prom {
 	private static Scanner sc;
 	public static void Promedios () {
@@ -9,43 +7,44 @@ public class Prom {
 		boolean aux = true;
 		String num = new String ("");
 		while (aux == true) {
-			System.out.print("¿Se tiene una cantidad fija de estudiantes?"
-					+"\n"+"	1) Si."+"\n"+"	2) No."+"\n"+"	3) Atras.");
+			System.out.print("¿Se tiene una cantidad fija de cursos,"
+					+ " estudiantes y notas?"
+					+"\n"+"	1) Si."+"\n"+"	2) No."+"\n"+"	3) Atras."+"\n");
 			num = sc.next();
 			switch (num) {
 			case "1": Prom_True(); break;
 			case "2": Prom_False(); break;
-			case "3": System.out.println(". . .Atras. . ."); aux = false; break;
+			case "3": System.out.println(". . . Atras . . ."); aux = false; break;
 			default: System.out.println("Opción no válida."); break;
 			}
 		}
 	}
 	private static void Prom_True () {
 		sc = new Scanner(System.in);
-		boolean aux = true;
-		System.out.println("Ingrese la cantidad de estudiantes.");
-		int est = sc.nextInt();
-		double [] cantidades = new double [est];
 	}
 	private static void Prom_False () {
 		sc = new Scanner(System.in);
-		boolean aux = true;
-		String num = new String ("");
-		System.out.print("Ingrese la cantidad de notas de los estudiantes."+
-		"\n"+"Una vez acabe de ingresar notas, escriba [END] y presione [ENTER]."
-		+"\n");
-		while(aux == true){
-			num = sc.next();
-			num = num.toUpperCase();
-			if (num.equals("END")) {
-				aux = false;
+	}
+	public static double Delta (String valor) {
+		String arreglar = new String ("");
+		for(int i = 0; i < valor.length(); i++) {
+			switch (valor.substring(i, i+1)){
+			case "0": arreglar += "0"; break;
+			case "1": arreglar += "1"; break;
+			case "2": arreglar += "2"; break;
+			case "3": arreglar += "3"; break;
+			case "4": arreglar += "4"; break;
+			case "5": arreglar += "5"; break;
+			case "6": arreglar += "6"; break;
+			case "7": arreglar += "7"; break;
+			case "8": arreglar += "8"; break;
+			case "9": arreglar += "9"; break;
+			case ",": arreglar += "."; break;
+			case ".": arreglar += "."; break;
+			default: break;
 			}
 		}
-	}
-	private static void Prom_Know () {
-		
-	}
-	private static void Prom_Unknow () {
-		
+		double resultado = Double.valueOf(arreglar);
+		return resultado;
 	}
 }
