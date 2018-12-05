4e99c95249222e6ad52b2689e1d3c8f72bc72af3
Enurghia <44556187+Enurghia@users.noreply.github.com>
2018-12-05 18:52:28 -0300

    Add files via upload

--- Calculeitor_Del_Futureichon/src/Cal/Cal_Class.java
+++ Calculeitor_Del_Futureichon/src/Cal/Cal_Class.java
@@ -20,21 +20,17 @@ public class Cal_Class {
 			switch(option){
 				case "+":
 					System.out.print("Ingrese un valor para someterlo a operación."+"\n");
-					A = sc.nextDouble();
+					A = sc.nextInt();
 					System.out.println("Ingrese otro valor para continuar la operación.");
-					B = sc.nextDouble();
-					String X = new String (""+(A+B));
-					X = String.format("%.10g%n", (A+B));
-					System.out.println(A+" + "+B+" = "+X+"\n");
+					B = sc.nextInt();
+					System.out.println(A+" + "+B+" = "+(A+B)+"\n");
 					break;
 				case "-":
 					System.out.print("Ingrese un valor para someterlo a operación."+"\n");
 					A = sc.nextDouble();
 					System.out.println("Ingrese otro valor para continuar la operación.");
 					B = sc.nextDouble();
-					String Y = new String (""+(A-B));
-					X = String.format("%.10g%n", (A-B));
-					System.out.println(A+" - "+B+" = "+Y+"\n");
+					System.out.println(A+" - "+B+" = "+(A-B)+"\n");
 					break;
 				case "*":
 					System.out.print("Ingrese un valor para someterlo a operación."+"\n");
@@ -48,12 +44,7 @@ public class Cal_Class {
 					A = sc.nextDouble();
 					System.out.println("Ingrese otro valor para continuar la operación.");
 					B = sc.nextDouble();
-					if (B==0){
-						System.out.println("\nError.\nNo se puede dividir por 0.\n");
-					}
-					else {
 					System.out.println(A+" / "+B+" = "+(Opera.division(A, B))+"\n");
-					}
 					break;
 				case "%":
 					System.out.print("Ingrese un valor para someterlo a operación."+"\n");
